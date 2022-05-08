915eae6e6cbe5f5725826010f8f55582156fb044
Ajanth Uthayan <uthayakumaran.ajanth@gmail.com>
2022-05-08 12:51:27 -0400

    fetch floor price of searched collection via client side

--- src/components/NFTMain/index.js
+++ src/components/NFTMain/index.js
@@ -1,11 +1,13 @@
 import NFTInfo from "../NFTInfo";
+import FloorPrice from "../FloorPrice";
 import SaleInput from "../SaleInput";
 
 function NFTMain(props) {
-  const { imageLink, name, salePrice } = props;
+  const { imageLink, name, salePrice, floorPrice } = props;
   return (
     <div>
       <NFTInfo imageLink={imageLink} name={name} />
+      <FloorPrice floorPrice={floorPrice}/>
       <SaleInput salePrice={salePrice} />
     </div>
   );
